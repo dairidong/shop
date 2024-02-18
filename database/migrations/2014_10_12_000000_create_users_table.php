a2b44c8810e001cb7540d1bee8a783325c11c9c6
戴日东 <550398445@qq.com>
2024-02-18 15:12:20 +0800

    user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->comment('用户表');
+
             $table->id();
-            $table->string('name');
+            $table->string('name')->comment('用户名');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable()->comment('邮箱验证时间');
+            $table->string('password')->comment('密码');
+            $table->string('avatar')->nullable()->comment('头像');
             $table->rememberToken();
             $table->timestamps();
         });
