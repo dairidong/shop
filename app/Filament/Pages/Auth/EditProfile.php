fddbcf7e7ca435396409eb0b4685cdf076decf8b
戴日东 <550398445@qq.com>
2024-02-22 22:31:51 +0800

    admin edit profile

---
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use \Filament\Pages\Auth\EditProfile as FilamentEditProfile;
+
+class EditProfile extends FilamentEditProfile
+{
+    /**
+     * @return array<int | string, string | Form>
+     */
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getUsernameFormComponent(),
+                        $this->getNameFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                    ])
+                    ->operation('edit')
+                    ->model($this->getUser())
+                    ->statePath('data')
+                    ->inlineLabel(! static::isSimple()),
+            ),
+        ];
+    }
+
+    protected function getUsernameFormComponent(): Component
+    {
+        return TextInput::make('username')
+            ->label(__('Username'))
+            ->disabled();
+    }
+}
