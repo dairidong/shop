8a5a147c25d06ec364137d1898b75ab1cd2fe454
戴日东 <550398445@qq.com>
2024-02-22 16:44:59 +0800

    fix user factory and test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -15,10 +15,10 @@ test('registration screen can be rendered', function () {
 
 test('new users can register', function () {
     $component = Volt::test('pages.auth.register')
-        ->set('name', 'Test User')
+        ->set('name', 'TestUser')
         ->set('email', 'test@example.com')
-        ->set('password', 'password')
-        ->set('password_confirmation', 'password');
+        ->set('password', 'password123')
+        ->set('password_confirmation', 'password123');
 
     $component->call('register');
 
