d04d4b896dedd2d365096424e1a645124a3e200b
戴日东 <550398445@qq.com>
2024-04-05 17:24:59 +0800

    user address list

---
+++ app/Livewire/UserAddress/UserAddressList.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\UserAddress;
+
+use App\Models\UserAddress;
+use Livewire\Component;
+
+class UserAddressList extends Component
+{
+    public function delete(UserAddress $userAddress)
+    {
+        $this->authorize('delete', $userAddress);
+
+        $userAddress->delete();
+
+        $this->dispatch('close-modal', 'delete-address-confirm-' . $userAddress->id);
+    }
+
+    public function render()
+    {
+        return view('livewire.user-address.user-address-list');
+    }
+}
